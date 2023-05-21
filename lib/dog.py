db4d7a9a8b982f588c5158a4814a049cb69d0f42
mohammed-khalif <mohamed.yusuf@student.moringaschool.com>
2023-05-21 15:42:53 -0400

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -2,3 +2,15 @@
 
 class Dog:
     pass
+# lib/dog.py
+
+class Dog:
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print(f"{self.name} is barking.")
+
+    def sit(self):
+        print(f"The dog {self.name} is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -2,3 +2,8 @@
 
 class Person:
     pass
+# lib/person.py
+
+class Person:
+    def __init__(self, name):
+        self.name = name
